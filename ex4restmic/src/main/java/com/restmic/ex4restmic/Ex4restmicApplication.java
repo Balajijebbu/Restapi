301c0e6b608e3ecb425dad1f5da957ade4bade65
Balajijebbu <jeyabalajichandrasekaran@gmail.com>
2023-10-25 20:09:39 +0530

    first commit

---
+++ ex4restmic/src/main/java/com/restmic/ex4restmic/Ex4restmicApplication.java
@@ -0,0 +1,12 @@
+package com.restmic.ex4restmic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex4restmicApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Ex4restmicApplication.class, args);
+	}
+}
---
+++ ex4restmic/src/main/java/com/restmic/ex4restmic/Model/User.java
@@ -0,0 +1,31 @@
+package com.restmic.ex4restmic.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="Blog") //Optional(Table name)
+
+public class User
+{
+     @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long id;
+     private String Name;
+    private String Mailid;
+    private String Dob;
+    private String Gender;
+    private String Qualification;
+    private String Feedback; 
+}
+
